06879b2a9d48127d1b99568d7fce6f7c9cffd90a
thucduy0103 <thucduy0103@gmail.com>
2021-11-14 22:51:39 +0700

    Updates

---
+++ precache-manifest.d53ccd6eb30006dc9c162074d00b3f7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "582bbe8a961bd26d1e074cc35fba010e",
+    "url": "/Medicine_api/index.html"
+  },
+  {
+    "revision": "93140480be05bd73b37c",
+    "url": "/Medicine_api/static/css/2.c33d3e7c.chunk.css"
+  },
+  {
+    "revision": "92417c27fde70089dbf7",
+    "url": "/Medicine_api/static/css/main.5bc842d9.chunk.css"
+  },
+  {
+    "revision": "93140480be05bd73b37c",
+    "url": "/Medicine_api/static/js/2.4637da2f.chunk.js"
+  },
+  {
+    "revision": "92417c27fde70089dbf7",
+    "url": "/Medicine_api/static/js/main.83adb023.chunk.js"
+  },
+  {
+    "revision": "0afb549bb33dcc242f53",
+    "url": "/Medicine_api/static/js/runtime-main.313cb8b9.js"
+  }
+]);
