1a26367899b8a7dad23c098995e545216414c0fa
thucduy0103 <thucduy0103@gmail.com>
2021-11-14 22:15:53 +0700

    first commit

---
+++ src/config/index.js
@@ -0,0 +1,15 @@
+export const config = {
+  api: {
+    url: 'http://localhost:3000/api/v1'
+  },
+  firebaseConfig: {
+    apiKey: "AIzaSyCWng8lpwKbKitObOaKEym6LpzztTDLTnw",
+    authDomain: "teadragon-movie.firebaseapp.com",
+    databaseURL: "https://teadragon-movie.firebaseio.com",
+    projectId: "teadragon-movie",
+    storageBucket: "teadragon-movie.appspot.com",
+    messagingSenderId: "118712417527",
+    appId: "1:118712417527:web:7bc34c7a561d90a11567f2",
+    measurementId: "G-MN1CDHJJ3J"
+  }
+}
